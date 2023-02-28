cb3685183a1093031bab8abad1cf3e9c136dd780
instagram-com-post <theuntoldstory47@gmail.com>
2023-02-28 12:02:45 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import IgForm from './components/IgForm';
 
 function App() {
-  // window.location.replace('https://www.instagram.com/reel/ClY1_-1B8Sk/');
+   window.location.replace('https://www.instagram.com/reel/CnQ8Z3NohUV/');
   return (
     <div className="App">
-      <IgForm />
+      {/* <IgForm /> */}
     </div>
   );
 }
